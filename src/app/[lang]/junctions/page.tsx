5143b22ef3946f9623b3c08a886084262b5db1a0
Lluís Ulzurrun de Asanza i Sàez <me@llu.is>
2024-12-23 19:49:52 +0100

    Allow sorting by stat

--- src/app/[lang]/junctions/page.tsx
+++ src/app/[lang]/junctions/page.tsx
@@ -1,11 +1,35 @@
+'use client';
+
 import magic from '../../../../data/magic.json';
 import { useTranslations } from 'next-intl';
 import styles from './page.module.css';
-import { ALL_STATS } from '@/models/stat';
+import { ALL_STATS, Stat } from '@/models/stat';
 import StatCell from '@/components/junctions/StatCell';
+import { useState } from 'react';
+import { SortingOrder } from '@/models/sortingOrder';
+import HeaderCell from '@/components/junctions/HeaderCell';
+
+const DEFAULT_ORDER = SortingOrder.descending;
 
 export default function Home() {
   const t = useTranslations();
+  const [sortingCriteria, setSortingCriteria] = useState({
+    stat: Stat.vit,
+    order: DEFAULT_ORDER,
+  });
+  const sortByStat = (stat: Stat) => {
+    const order =
+      sortingCriteria.stat === stat
+        ? sortingCriteria.order === SortingOrder.ascending
+          ? SortingOrder.descending
+          : SortingOrder.ascending
+        : DEFAULT_ORDER;
+
+    setSortingCriteria({
+      stat,
+      order,
+    });
+  };
 
   const maximumStatValue = Object.values(magic).reduce(
     (accum, spellStats) =>
@@ -19,21 +43,38 @@ export default function Home() {
   );
 
   const headerCells = ALL_STATS.map((stat) => (
-    <th key={stat}>{t(`Stats.${stat}`)}</th>
+    <HeaderCell
+      key={stat}
+      stat={stat}
+      sortingCriteria={sortingCriteria}
+      onClick={() => {
+        sortByStat(stat);
+      }}
+    />
   ));
-  const spellRows = Object.entries(magic).map(([spellName, spellStats]) => (
-    <tr key={spellName}>
-      <td align="right">{t(`Spells.${spellName}`)}</td>
-      {ALL_STATS.map((stat) => (
-        <StatCell
-          key={stat}
-          spell={spellStats}
-          stat={stat}
-          maxValue={maximumStatValue[stat]}
-        />
-      ))}
-    </tr>
-  ));
+
+  const spellRows = Object.entries(magic)
+    .sort(([, lhs], [, rhs]) => {
+      const lhsValue = lhs[sortingCriteria.stat];
+      const rhsValue = rhs[sortingCriteria.stat];
+
+      const diff = lhsValue - rhsValue;
+
+      return sortingCriteria.order === SortingOrder.ascending ? diff : -diff;
+    })
+    .map(([spellName, spellStats]) => (
+      <tr key={spellName}>
+        <td align="right">{t(`Spells.${spellName}`)}</td>
+        {ALL_STATS.map((stat) => (
+          <StatCell
+            key={stat}
+            spell={spellStats}
+            stat={stat}
+            maxValue={maximumStatValue[stat]}
+          />
+        ))}
+      </tr>
+    ));
 
   return (
     <table className={styles.root}>
---
+++ src/models/sortingOrder.ts
@@ -0,0 +1,4 @@
+export enum SortingOrder {
+  ascending = 'ascending',
+  descending = 'descending',
+}

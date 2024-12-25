42c8037436bb1afbad2e2a1b74b4fee65e22132f
Lluís Ulzurrun de Asanza Sàez <me@llu.is>
2024-12-25 22:56:15 +0100

    Add spell filter to all junctions page

--- src/app/[lang]/junctions/all/page.tsx
+++ src/app/[lang]/junctions/all/page.tsx
@@ -1,5 +1,6 @@
 import { useTranslations } from 'next-intl';
 import AllSpellsTable from '@/components/spells/AllSpellsTable';
+import SpellFilter from '@/components/spells/SpellFilter';
 import H2 from '@/components/typography/H2';
 
 export default function Home() {
@@ -8,6 +9,7 @@ export default function Home() {
   return (
     <>
       <H2>{t('JuctionsTable.title')}</H2>
+      <SpellFilter className="mb-3" />
       <div className="flex flex-row gap-4 overflow-x-auto">
         <AllSpellsTable />
       </div>
